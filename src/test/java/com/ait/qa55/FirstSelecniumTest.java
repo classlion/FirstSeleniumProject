22c26c16804eb9b73dad89adb870aa5d9e07cd7f
classlion <classlion1@gmail.com>
2025-04-04 10:06:37 +0200

    Add

---
+++ src/test/java/com/ait/qa55/FirstSelecniumTest.java
@@ -0,0 +1,54 @@
+package com.ait.qa55;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class FirstSelecniumTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+      driver = new ChromeDriver();
+      //driver.get("https://www.google.com");
+      //driver.navigate().to("https://www.google.com");
+        //maximize browser to windoiw
+        driver.manage().window().maximize();
+        driver.navigate().to("https://seferisrael.co.il");//with history
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.navigate().to("https://www.google.com");
+        driver.navigate().back();
+        driver.navigate().refresh();
+    }
+
+
+    @Test
+    public void openGoogleTest(){
+        System.out.println("Hello");
+    }
+
+    @AfterMethod (enabled = false)
+    public void tearDown(){
+
+        //driver.quit();
+        driver.close();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
