3abede978d457fed8c09c299bde17aa803b905e7
classlion <classlion1@gmail.com>
2025-04-07 19:08:49 +0200

    Add 04.04.

--- src/test/java/com/ait/qa55/HomeworkTest.java
+++ src/test/java/com/ait/qa55/HomeworkTest.java
@@ -22,12 +22,37 @@ public class HomeworkTest {
 
     @AfterMethod
     public  void tearDown(){
-
     }
 
     @Test
-    public void Console(){
-        System.out.println("Done");
+    public void FourAiprilHomework(){
+        driver.findElement(By.className("bar-notification"));
+        driver.findElement(By.className("footer"));
+        driver.findElement(By.className("header"));
+        driver.findElement(By.className("header-menu"));
+        driver.findElement(By.className("rss"));
+        driver.findElement(By.className("twitter"));
+        driver.findElement(By.className("google-plus"));
+        driver.findElement(By.className("youtube"));
+        driver.findElement(By.id("mob-menu-button"));
+        driver.findElement(By.id("ui-id-1"));
+    };
+
+    @Test
+    public void SevenAprilHomework(){
+
+        driver.findElements(By.cssSelector(".header"));
+        driver.findElements(By.cssSelector(".footer"));
+        driver.findElements(By.cssSelector(".h1"));
+        driver.findElement(By.cssSelector("#mob-menu-button"));
+        driver.findElement(By.cssSelector("#ui-id-1"));
+        driver.findElement(By.cssSelector("[href$='/search']"));
+        driver.findElement(By.cssSelector("[href^='/sea']"));
+        driver.findElement(By.cssSelector("[href^='/Them']"));
+        driver.findElement(By.cssSelector("img[src$='images/logo.png']"));
+        driver.findElement(By.cssSelector("img[src$='png']"));
+
+
 
     }
 }
